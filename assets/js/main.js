3bc6b06089c4ba560993d58bc6f9e8508ceb5a65
dailybootstrap7 <dailybootstrap.gmail.com>
2018-05-24 19:59:15 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+$(function() {
+
+	 var swiper = new Swiper('.swiper-container.recom-container', {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      }
+
+
+    });
+	
+});
